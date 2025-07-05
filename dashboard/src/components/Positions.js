25bf1353c134706c7efd1869510f973ff303a7d4
Viraj Mulik <144205690+Viraj1923@users.noreply.github.com>
2025-07-05 17:58:12 +0530

    Update Positions.js

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -7,7 +7,7 @@ const Positions = () => {
   const [allPositions,setAllPositions]=useState([]);
 
   useEffect(()=>{
-    axios.get("http://localhost:3002/allPositions").then((res)=>{
+    axios.get("https://zerodha-backend-qk9h.onrender.com/allPositions").then((res)=>{
       setAllPositions(res.data);
     });
   },[]);
